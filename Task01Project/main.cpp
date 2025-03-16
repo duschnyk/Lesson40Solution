8e24b2ad55f37e21125733e840a68503fa3a7833
giganig <matveykaschuk@gmail.com>
2025-03-16 18:52:04 +0300

    Add project files.

---
+++ Task01Project/main.cpp
@@ -0,0 +1,21 @@
+#include "util.h"
+#include "logic.h"
+#define SIZE 10
+
+int main() {
+	int array[SIZE]{ 9,8,7,6,5,4,3,2,1,0 };
+
+		//init(array, SIZE, -50, 50);
+
+	cout << "Array before: " << convert(array, SIZE) << endl;
+
+	bubble_sort(array, SIZE, true);
+
+	cout << "Array after: " << convert(array, SIZE) << endl;
+
+	bubble_sort(array, SIZE, false);
+
+	cout << "Array after_after: " << convert(array, SIZE) << endl;
+
+	return 0;
+}
