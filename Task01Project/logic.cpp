4ca2afcccb3f886252940cbf088d318234189004
giganig <matveykaschuk@gmail.com>
2025-03-16 19:42:52 +0300

    Update

--- Task01Project/logic.cpp
+++ Task01Project/logic.cpp
@@ -1,18 +1,21 @@
 #include "logic.h"
-#include "util.h"
+
 void bubble_sort(int* array, int size, bool type) {
-	int count = 0;
 	for (int j = 0; j < size - 1; j++)
 	{
-		for (int i = 0; i < size - 1; i++)
+		bool flag = true;
+		
+		for (int i = 0; i < size - 1 - j; i++)
 		{
-			count++;
 			if (type ? array[i] < array[i + 1] : array[i] > array[i + 1]) {
+				flag = false;
 				int t = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = t;
 			}
 		}
+		if (flag) {
+			break;
+		}
 	}
-	cout << "Count: " << count << endl;
 }
